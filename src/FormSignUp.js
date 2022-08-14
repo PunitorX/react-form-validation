95b094994d8c13eaa714181b9ff52a30de8765e7
Robert Jones <jones.robertbrendan@gmail.com>
2022-08-14 14:47:06 -0500

    'Updated jsx and added some css'

--- src/FormSignUp.js
+++ src/FormSignUp.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import useForm from './useForm'
+import validateInfo from './validateInfo'
+import './Form.css'
 
-function FormSignUp() {
+function FormSignUp({submitForm}) {
+    const {handleChange, values, handleSubmit, errors} = useForm(
+        submitForm, 
+        validateInfo)
+
   return (
     <div className="form-content-right">
-        <form className="form">
+        <form className="form" onSubmit={handleSubmit}>
             <h1>Get started with us today! Create your account by filling out the information below.</h1>
-            <div className="form-input">
+            <div className="form-inputs">
                 <label htmlFor="username" className="form-label">
                     Username
                 </label>
@@ -14,9 +21,12 @@ function FormSignUp() {
                         type="text" 
                         name='username' className="form-input" 
                         placeholder='Enter your username' 
+                        value={values.username}
+                        onChange={handleChange}
                     />
+                    {errors.username && <p>{errors.username}</p>}
             </div>
-            <div className="form-input">
+            <div className="form-inputs">
                 <label htmlFor="email" className="form-label">
                     Email
                 </label>
@@ -25,9 +35,12 @@ function FormSignUp() {
                         type="email" 
                         name='email' className="form-input" 
                         placeholder='Enter your email' 
+                        value={values.email}
+                        onChange={handleChange}
                     />
+                    {errors.email && <p>{errors.email}</p>}
             </div>
-            <div className="form-input">
+            <div className="form-inputs">
                 <label htmlFor="password" className="form-label">
                     Password
                 </label>
@@ -36,18 +49,24 @@ function FormSignUp() {
                         type="password" 
                         name='password' className="form-input" 
                         placeholder='Enter your password' 
+                        value={values.password}
+                        onChange={handleChange}
                     />
+                    {errors.password && <p>{errors.password}</p>}
             </div>
-            <div className="form-input">
+            <div className="form-inputs">
                 <label htmlFor="password2" className="form-label">
                     Confirm Password
                 </label>
                 <input  
                         id='password2'
-                        type="password2" 
+                        type="password" 
                         name='password2' className="form-input" 
                         placeholder='Enter your password2' 
+                        value={values.password2}
+                        onChange={handleChange}
                     />
+                    {errors.password2 && <p>{errors.password2}</p>}
             </div>
             <button className="form-input-btn" type='submit'>
                 Sign Up
